f2b7cc96be0ea5f18a73d1903a6a1cdbfbd9d2b8
BobbyShmurner <glitched.ohdeymus@gmail.com>
2022-07-13 14:34:59 +0100

    Added Transparent and Second Player Cubemaps

--- Plugin.cs
+++ Plugin.cs
@@ -8,7 +8,7 @@ using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Experimental.Rendering;
+using UnityEngine.SceneManagement;
 
 namespace CubemapMaker
 {
@@ -20,6 +20,7 @@ namespace CubemapMaker
 
         internal static ConfigFile config;
         internal static ConfigEntry<string> configCaptureKeybind;
+        internal static ConfigEntry<string> configTransCaptureKeybind;
         internal static ConfigEntry<string> configOutputPath;
         internal static ConfigEntry<string> configCaptureOrientation;
         internal static ConfigEntry<int> configOutputWidth;
@@ -64,7 +65,9 @@ namespace CubemapMaker
             configOutputPath = config.Bind("General", "OutputPath", outputFolder, "The path that cubemaps will be outputed to");
             configOutputWidth = config.Bind("General", "OutputWidth", 4096, "The width of the outputed cubemap");
             configCaptureOrientation = config.Bind("General", "CaptureOrientation", "Yaw", new ConfigDescription("The orientation used to capture the cubemap in\nYaw: This will capture the cubemap in the direction your facing but will keep the world upright\nAccurate: This will accuratly capture rotation in the cubemap, so whereever you are looking will be the forward position\nNone: This won't take the camera's orientation into account at all", new AcceptableValueList<string>("Yaw", "Accurate", "None")));
+
             configCaptureKeybind = config.Bind("General.Keybinds", "CaptureButton", "f10", "The key used to capture a cubemap");
+            configTransCaptureKeybind = config.Bind("General.Keybinds", "TransparentCaptureButton", "f11", "The key used to capture a cubemap with a transparent skybox");
 
             outputFolder = configOutputPath.Value;
 
@@ -73,48 +76,86 @@ namespace CubemapMaker
             Plugin.Log.LogInfo("Loaded Config");
         }
 
-        private void Update() {
-            if (!Input.GetKeyDown(configCaptureKeybind.Value.ToLower())) return;
+        private void CaptureCubemap(bool transparent, bool cgSecondPlayer = false) {
+            if (transparent) Plugin.Log.LogInfo("Capturing Transparent Cubemap");
+            else Plugin.Log.LogInfo("Capturing Cubemap");
+
+            if (cgSecondPlayer) {
+                if (SceneManager.GetActiveScene().name != "Endless") {
+                    cgSecondPlayer = false;
+                    
+                    Plugin.Log.LogWarning("Tried capturing a CG Second Player cubemap outside of the cyber grind. Capturing normal cubemap instead");
+                } else {
+                    Plugin.Log.LogInfo("Capturing CG Second Player Cubemap");
+                }
+            }
 
             string fileName = $"{Application.productName} {DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss")}.png";
             string filePath = Path.GetFullPath(Path.Combine(configOutputPath.Value, fileName));
 
+            // General Capture Vars
             RenderTexture cubeTex = new RenderTexture(configOutputWidth.Value, configOutputWidth.Value, 32);
             RenderTexture equirectTex = new RenderTexture(cubeTex.width, (int)(cubeTex.width * 0.5f), 32);
-            Texture2D tex = new Texture2D(equirectTex.width, equirectTex.height);
-
-            cubeTex.dimension = TextureDimension.Cube;
+            Texture2D capturedTex = new Texture2D(equirectTex.width, equirectTex.height);
             Quaternion originalRot = Camera.main.transform.rotation;
+            Vector3 originalPos = Camera.main.transform.position;
 
-            switch (configCaptureOrientation.Value.ToLower()) {
-                case "none":
-                    Camera.main.transform.eulerAngles = Vector3.zero;
-                    break;
-                case "accurate":
-                    break;
-                default:
-                    Camera.main.transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
-                    break;
+            // Transparent Vars
+            CameraClearFlags originalClearFlags = Camera.main.clearFlags;
+            Color originalColor = Camera.main.backgroundColor;
+
+            // Setup the correct capture orientation
+            if (!cgSecondPlayer) {
+                switch (configCaptureOrientation.Value.ToLower()) {
+                    case "none":
+                        Camera.main.transform.eulerAngles = Vector3.zero;
+                        break;
+                    case "accurate":
+                        break;
+                    default:
+                        Camera.main.transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
+                        break;
+                }
+            } else {
+                Camera.main.transform.eulerAngles = Vector3.zero; // Set orientation to none for cg second player
+                Camera.main.transform.position = new Vector3(500, 30, 50); // TODO: Let the player set how far away the camera is
             }
 
-            // Using A Stereoscopic Eye allows for roation to also be captured
-            // We rotate before capturing because the image is always rotated 90 degrees to the left
+            if (transparent) {
+                Camera.main.clearFlags = CameraClearFlags.Color;
+                Camera.main.backgroundColor = Color.clear;
+            }
 
+            // Capture the cubemap
+            cubeTex.dimension = TextureDimension.Cube;
             Camera.main.transform.Rotate(new Vector3(0, 90, 0));
             Camera.main.RenderToCubemap(cubeTex, 63, Camera.MonoOrStereoscopicEye.Left);
             cubeTex.ConvertToEquirect(equirectTex, Camera.MonoOrStereoscopicEye.Mono);
 
+            // Reset the camera to its original values
             Camera.main.transform.rotation = originalRot;
+            Camera.main.transform.position = originalPos;
             Camera.main.ResetAspect();
 
+            // Reset Transparent Settings. No point in branching here
+            Camera.main.clearFlags = originalClearFlags;
+            Camera.main.backgroundColor = originalColor;
+
+            // Render the cubemap to a Texture2D
             RenderTexture.active = equirectTex;
-            tex.ReadPixels(new Rect(0, 0, equirectTex.width, equirectTex.height), 0, 0);
+            capturedTex.ReadPixels(new Rect(0, 0, equirectTex.width, equirectTex.height), 0, 0);
             RenderTexture.active = null;
 
+            // Save the image to disk
             Directory.CreateDirectory(configOutputPath.Value);
-            File.WriteAllBytes(filePath, tex.EncodeToPNG());
+            File.WriteAllBytes(filePath, capturedTex.EncodeToPNG());
 
             Plugin.Log.LogInfo($"Captured cubemap to \"{filePath}\"");
+        }
+
+        private void Update() {
+            if (Input.GetKeyDown(configCaptureKeybind.Value.ToLower())) CaptureCubemap(false, Input.GetKey(KeyCode.LeftShift));
+            if (Input.GetKeyDown(configTransCaptureKeybind.Value.ToLower())) CaptureCubemap(true, Input.GetKey(KeyCode.LeftShift));
         }
     }
 }
--- README.md
+++ README.md
@@ -7,9 +7,21 @@ Create 360 Cubemaps from within Unity games for use in The Cyber Grind
 
 Press "F10" to capture a Cubemap
 
+Press "F11" to capture a Cubemap with a transparent skybox
+
+Holding down "Left Shift" while capturing a cubemap on The Cyber Grind will capture a [Second Player Cubemap](#second-player-cubemaps-the-cyber-grind)
+
 The image will be saved to The ULTRAKILL's Cyber Grind Skybox folder by default, but in edge cases where the plugin could not find this folder (or you aren't planning on using this for ULTRAKILL), it will be saved in a folder in your Pictures directory named "Cubemaps"
 
-You can find the `config.cfg` in `%AppData%/CubemapMaker/`. There you can change the output path, the capture key, the orientation of the cubemap, and the output resolution.
+You can find the `config.cfg` in `%AppData%/CubemapMaker/`. There you can change the output path, the capture key, the orientation of the cubemap, the output resolution, and whether or not to include the UI in a [Second Player Cubemap](#second-player-cubemaps-the-cyber-grind).
+
+## Second Player Cubemaps (The Cyber Grind)
+
+While in The Cyber grind, if you hold down shift while capturing a cubemap, a special "Second Player" cubemap will be created.
+
+These Second Player cubemaps are supposed to give the illusion that a second player is playing beside you. You could think of them as a rival, or as moral support. The choice is yours, my friend!
+
+[![Second Player Cubemap](img/competitor.png)](https://momento360.com/e/u/b6f03b9e647f49dcb43052df860e0e4e?utm_campaign=embed&utm_source=other&heading=-156.18&pitch=21.31&field-of-view=77&size=medium)
 
 ## Orientation Settings
 
